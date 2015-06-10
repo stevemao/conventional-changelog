eb5fc6969ac134cd63e5d7c56b8b70c523ce94d1
Steve Mao <maochenyan@gmail.com>
2015-06-10 17:44:16 +1000

    travis fail

--- test/angular.js
+++ test/angular.js
@@ -1,5 +1,4 @@
 'use strict';
-var child = require('child_process');
 var conventionalChangelog = require('../');
 var expect = require('chai').expect;
 var shell = require('shelljs');
@@ -14,15 +13,13 @@ describe('presets', function() {
       writeFileSync('test1', '');
       shell.exec('git add --all && git commit -m"chore: first commit"');
       writeFileSync('test2', '');
-      // fix this until https://github.com/arturadib/shelljs/issues/175 is solved
-      child.exec('git add --all && git commit -m"feat: amazing new module\n\nBREAKING CHANGE: Not backward compatible."', function() {
-        writeFileSync('test3', '');
-        shell.exec('git add --all && git commit -m"fix($compile): avoid a bug"');
-        writeFileSync('test4', '');
-        shell.exec('git add --all && git commit -m"perf(ngOptions): make it faster"');
-        writeFileSync('test5', '');
-        shell.exec('git add --all && git commit -m"revert(ngOptions): make it faster"');
-      });
+      shell.exec('git add --all && git commit -m\$\'feat: amazing new module\\n\\nBREAKING CHANGE: Not backward compatible.\'');
+      writeFileSync('test3', '');
+      shell.exec('git add --all && git commit -m"fix($compile): avoid a bug"');
+      writeFileSync('test4', '');
+      shell.exec('git add --all && git commit -m"perf(ngOptions): make it faster"');
+      writeFileSync('test5', '');
+      shell.exec('git add --all && git commit -m"revert(ngOptions): make it faster"');
     });
 
     after(function() {
